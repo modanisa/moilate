53bf186dfd9ae4e7f48222d42cf675213918f84c
Taylan Kasap <taylan.kasap@modanisa.com>
2021-09-28 16:49:34 +0300

    add more tests for UIAutomatorSelector

--- src/__tests__/selectors/UIAutomatorSelector.test.ts
+++ src/__tests__/selectors/UIAutomatorSelector.test.ts
@@ -33,4 +33,66 @@ describe('UIAutomatorSelector', () => {
       uiAutomatorSelector.contains('test-string', 'unknown-property');
     }).toThrowError();
   });
+
+  test('contains method generates correct selector string when used multiple times with and method', () => {
+    const uiAutomatorSelector = new UIAutomatorSelector(null);
+
+    uiAutomatorSelector
+      .contains('test-string', SelectorProperty.Android.UIAutomator.DESCRIPTION)
+      .and()
+      .contains('test-string-2', SelectorProperty.Android.UIAutomator.TEXT)
+      .and()
+      .contains('test-string-3', SelectorProperty.Android.UIAutomator.RESOURCE_ID);
+
+    expect(uiAutomatorSelector.selector).toBe(
+      `android=new UiSelector().descriptionContains("test-string").textContains("test-string-2").resourceIdMatches("^.*test-string-3.*$")`,
+    );
+  });
+
+  test('exactMatch method generates correct selector string for text property', () => {
+    const uiAutomatorSelector = new UIAutomatorSelector(null);
+
+    uiAutomatorSelector.exactMatch('test-string', SelectorProperty.Android.UIAutomator.TEXT);
+
+    expect(uiAutomatorSelector.selector).toBe(`android=new UiSelector().text("test-string")`);
+  });
+
+  test('exactMatch method generates correct selector string for text property', () => {
+    const uiAutomatorSelector = new UIAutomatorSelector(null);
+
+    uiAutomatorSelector.exactMatch('test-string', SelectorProperty.Android.UIAutomator.RESOURCE_ID);
+
+    expect(uiAutomatorSelector.selector).toBe(`android=new UiSelector().resourceId("test-string")`);
+  });
+
+  test('exactMatch method generates correct selector string for text property', () => {
+    const uiAutomatorSelector = new UIAutomatorSelector(null);
+
+    uiAutomatorSelector.exactMatch('test-string', SelectorProperty.Android.UIAutomator.DESCRIPTION);
+
+    expect(uiAutomatorSelector.selector).toBe(`android=new UiSelector().description("test-string")`);
+  });
+
+  test('exactMatch method throws error when unknown property is given', () => {
+    const uiAutomatorSelector = new UIAutomatorSelector(null);
+
+    expect(() => {
+      uiAutomatorSelector.exactMatch('test-string', 'unknown-property');
+    }).toThrowError();
+  });
+
+  test('exactMatch method generates correct selector string when used multiple times with and method', () => {
+    const uiAutomatorSelector = new UIAutomatorSelector(null);
+
+    uiAutomatorSelector
+      .exactMatch('test-string', SelectorProperty.Android.UIAutomator.DESCRIPTION)
+      .and()
+      .exactMatch('test-string-2', SelectorProperty.Android.UIAutomator.TEXT)
+      .and()
+      .exactMatch('test-string-3', SelectorProperty.Android.UIAutomator.RESOURCE_ID);
+
+    expect(uiAutomatorSelector.selector).toBe(
+      `android=new UiSelector().description("test-string").text("test-string-2").resourceId("test-string-3")`,
+    );
+  });
 });
